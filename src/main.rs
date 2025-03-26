cd12e021264bf92ef44bc5e45a1e6c47dd2672af
bourbonut <ben.bourbon06@gmail.com>
2025-03-26 18:48:26 +0100

    Incorporate all colors directly in the code

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,26 @@
 //    println!("{:?}", colors.get(&(-1)));
 //}
 
-mod schemes;
+mod colors;
 
 use ggez::conf::WindowMode;
+use ggez::conf::WindowSetup;
 use ggez::event;
 use ggez::glam::*;
-use ggez::graphics::Drawable;
-use ggez::graphics::FontData;
-use ggez::graphics::PxScale;
-use ggez::graphics::TextFragment;
-use ggez::graphics::{self, Rect};
+use ggez::graphics::{
+    Canvas, DrawMode, Drawable, FontData, Mesh, PxScale, Rect, Text, TextFragment,
+};
 use ggez::{Context, GameResult};
 use std::fs::File;
 use std::io::Read;
 
-use schemes::Scheme;
+use colors::{GameColor, BACKGROUND, SPRITES};
 
 struct MainState {
     pos_x: f32,
     locations: Vec<Vec2>,
-    scheme: Scheme,
+    background: GameColor,
+    game_colors: [GameColor; 18],
 }
 
 impl MainState {
@@ -37,6 +37,7 @@ impl MainState {
         file.read_to_end(&mut buf)?;
         let font = FontData::from_vec(buf)?;
         ctx.gfx.add_font("ClearSans-Bold", font);
+
         let s = MainState {
             pos_x: 0.0,
             locations: (0..4)
@@ -44,7 +45,8 @@ impl MainState {
                     (0..4).map(move |j| Vec2::new((15 + 121 * j) as f32, (15 + 121 * i) as f32))
                 })
                 .collect(),
-            scheme: Scheme::new(),
+            background: GameColor::new(BACKGROUND),
+            game_colors: SPRITES.map(|sprite| GameColor::new(sprite)),
         };
         Ok(s)
     }
@@ -57,24 +59,23 @@ impl event::EventHandler<ggez::GameError> for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas =
-            graphics::Canvas::from_frame(ctx, graphics::Color::from_rgb(183, 173, 160));
+        let mut canvas = Canvas::from_frame(ctx, self.background.rgb);
 
         for (i, location) in self.locations.iter().enumerate() {
             let number = 2_i64.pow(i as u32 + 1);
-            let sprite_desc = self.scheme.get(&number).unwrap();
-            let rect = graphics::Mesh::new_rounded_rectangle(
+            let game_color = &self.game_colors[i + 1];
+            let rect = Mesh::new_rounded_rectangle(
                 ctx,
-                graphics::DrawMode::fill(),
+                DrawMode::fill(),
                 Rect::new(0., 0., 105., 105.),
                 5.,
-                sprite_desc.rgb,
+                game_color.rgb,
             )?;
-            let text = graphics::Text::new(
+            let text = Text::new(
                 TextFragment::new(format!("{}", number))
                     .font("ClearSans-Bold")
-                    .color(sprite_desc.font_color)
-                    .scale(PxScale::from(sprite_desc.size as f32)),
+                    .color(game_color.font_color)
+                    .scale(PxScale::from(game_color.size as f32)),
             );
             let [w, h] = text.dimensions(ctx).unwrap().center().into();
             canvas.draw(&rect, *location);
@@ -90,8 +91,13 @@ impl event::EventHandler<ggez::GameError> for MainState {
 }
 
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez")
-        .window_mode(WindowMode::default().dimensions(500., 500.));
+    let cb = ggez::ContextBuilder::new("game2048", "bourbonut")
+        .window_mode(WindowMode::default().dimensions(500., 500.))
+        .window_setup(
+            WindowSetup::default()
+                .title("2048"),
+                //.icon("logo.png"),
+        );
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
