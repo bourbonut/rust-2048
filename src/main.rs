963ea5d962510e4cbdda3ac0cd6a38ca8daf6ebd
bourbonut <ben.bourbon06@gmail.com>
2025-03-27 11:08:25 +0100

    Reorganize colors into cells for better loading

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,36 @@ struct MainState {
     pos_x: f32,
     locations: Vec<Vec2>,
     background: GameColor,
-    game_colors: [GameColor; 18],
+    cells: [Cell; 18],
+}
+
+struct Cell {
+    pub rect: Mesh,
+    pub text: Text,
+}
+
+impl Cell {
+    fn new(ctx: &mut Context, number: u32, game_color: GameColor) -> GameResult<Self> {
+        let number = if number != 0 {
+            2_i64.pow(number)
+        } else {
+            0_i64
+        };
+        let rect = Mesh::new_rounded_rectangle(
+            ctx,
+            DrawMode::fill(),
+            Rect::new(0., 0., 105., 105.),
+            5.,
+            game_color.rgb,
+        )?;
+        let text = Text::new(
+            TextFragment::new(format!("{}", number))
+                .font("ClearSans-Bold")
+                .color(game_color.font_color)
+                .scale(PxScale::from(game_color.size as f32)),
+        );
+        Ok(Cell { rect, text })
+    }
 }
 
 impl MainState {
@@ -35,6 +64,7 @@ impl MainState {
         let font = FontData::from_path(&ctx.fs, PathBuf::from("/clear-sans.bold.ttf"))?;
         ctx.gfx.add_font("ClearSans-Bold", font);
 
+        let mut number: u32 = 0;
         let s = MainState {
             pos_x: 0.0,
             locations: (0..4)
@@ -43,7 +73,11 @@ impl MainState {
                 })
                 .collect(),
             background: GameColor::new(BACKGROUND),
-            game_colors: SPRITES.map(|sprite| GameColor::new(sprite)),
+            cells: SPRITES.map(|sprite| {
+                let cell = Cell::new(ctx, number, GameColor::new(sprite)).unwrap();
+                number += 1;
+                cell
+            }),
         };
         Ok(s)
     }
@@ -59,25 +93,13 @@ impl event::EventHandler<ggez::GameError> for MainState {
         let mut canvas = Canvas::from_frame(ctx, self.background.rgb);
 
         for (i, location) in self.locations.iter().enumerate() {
-            let number = 2_i64.pow(i as u32 + 1);
-            let game_color = &self.game_colors[i + 1];
-            let rect = Mesh::new_rounded_rectangle(
-                ctx,
-                DrawMode::fill(),
-                Rect::new(0., 0., 105., 105.),
-                5.,
-                game_color.rgb,
-            )?;
-            let text = Text::new(
-                TextFragment::new(format!("{}", number))
-                    .font("ClearSans-Bold")
-                    .color(game_color.font_color)
-                    .scale(PxScale::from(game_color.size as f32)),
-            );
+            let cell = &self.cells[i + 1];
+            let rect = &cell.rect;
+            let text = &cell.text;
             let [w, h] = text.dimensions(ctx).unwrap().center().into();
-            canvas.draw(&rect, *location);
+            canvas.draw(rect, *location);
             canvas.draw(
-                &text,
+                text,
                 *location + Vec2::new((53 - w as i32 - 2) as f32, (53 - h as i32 - 5) as f32),
             );
         }
@@ -91,8 +113,10 @@ pub fn main() -> GameResult {
     let current_path = current_dir().unwrap();
     let resources_path = current_path.join(PathBuf::from("resources"));
     if !resources_path.exists() {
-        panic!("\nIn order to access correctly to resources, you must run the following command\
-            from the root of the project:\n$ cargo run\n")
+        panic!(
+            "\nIn order to access correctly to resources, you must run the following command\
+            from the root of the project:\n$ cargo run\n"
+        )
     }
     let cb = ggez::ContextBuilder::new("game2048", "bourbonut")
         .add_resource_path(resources_path)
