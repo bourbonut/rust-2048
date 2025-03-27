4c74de63506256ee98818f3f0a33f21d313c6a5a
bourbonut <ben.bourbon06@gmail.com>
2025-03-27 10:25:32 +0100

    Add panic message when the program is not run at the correct location

--- src/main.rs
+++ src/main.rs
@@ -90,13 +90,16 @@ impl event::EventHandler<ggez::GameError> for MainState {
 pub fn main() -> GameResult {
     let current_path = current_dir().unwrap();
     let resources_path = current_path.join(PathBuf::from("resources"));
+    if !resources_path.exists() {
+        panic!("\nIn order to access correctly to resources, you must run the following command\
+            from the root of the project:\n$ cargo run\n")
+    }
     let cb = ggez::ContextBuilder::new("game2048", "bourbonut")
         .add_resource_path(resources_path)
         .window_mode(WindowMode::default().dimensions(500., 500.))
         .window_setup(
-            WindowSetup::default()
-                .title("2048"),
-                //.icon("/logo.png"),
+            WindowSetup::default().title("2048"),
+            //.icon("/logo.png"),
         );
 
     let (mut ctx, event_loop) = cb.build()?;
